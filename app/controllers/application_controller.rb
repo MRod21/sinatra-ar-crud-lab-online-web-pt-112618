3b6c4237ac52481a9e01f65f7f5b0e258e2d999e
Mario Rodriguez <mario.rodriguez35@yahoo.com>
2019-03-31 21:12:16 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -24,7 +24,7 @@ class ApplicationController < Sinatra::Base
   
   post '/articles' do 
     @article = Article.create(params)
-    redirect "/articles/#{ @article.id }"
+    redirect "/articles/#{@article.id}"
   end
   
   
